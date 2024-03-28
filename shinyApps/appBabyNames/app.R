104723db0dc6fb26004c04e6ca0022415715e141
Morgen K <morgenk@reed.edu>
2024-03-28 16:28:01 -0700

    lab + prac

--- shinyApps/appBabyNames/app.R
+++ shinyApps/appBabyNames/app.R
@@ -5,12 +5,79 @@ library(DT)
 
 # User interface
 
-ui <- fluidPage()
-
+# User interface
+ui <- fluidPage(
+  titlePanel("Which Math 241 name is most popular?"),
+  sidebarLayout(
+    sidebarPanel(
+      # Create a text input widget
+      selectizeInput(inputId = "names",
+                     label = "Enter Math 241 names here",
+                     choices = NULL,
+                     multiple = TRUE),
+      radioButtons(inputId = "variable",
+                   label = "Variable of Interest",
+                   choices = c("n", "prop"),
+                   selected = "prop"),
+      p("Put single space between the names."),
+      # Create slider widget
+      sliderInput("year_range", "Range of Years:",
+                  min = min(babynames$year), 
+                  max = max(babynames$year),
+                  value = c(1980, 2010),
+                  sep = ""),
+      submitButton("Update Results!")
+    ),
+    mainPanel(
+      plotOutput(outputId = "graph"),
+      dataTableOutput(outputId = "table")
+    )
+  )
+)
 
 # Server function
 
-server <- function(input, output){
+server <- function(input, output, session){
+  
+  updateSelectizeInput(session, 'names', 
+                       choices = unique(babynames$name), 
+                       server = TRUE)
+  
+  dat_names <- reactive({ 
+    babynames %>%
+      group_by(year, name) %>%
+      summarize(n = sum(n)) %>%
+      group_by(year) %>%
+      mutate(prop = n/sum(n)) %>%
+      filter(name %in% c(unlist(str_split(input$names, " "))),
+             year >= input$year_range[1], 
+             year <= input$year_range[2]) 
+    })
+  
+  output$graph <- renderPlot({
+    
+    ggplot(data = dat_names(), 
+           mapping = aes(x = year, y = .data[[input$variable]],
+                         color = name)) +
+      geom_line(size = 2)
+  })  
+  
+  
+  dat_names_agg <- reactive({ 
+    dat_names() %>%
+      group_by(name) %>%
+      summarize(count = sum(n)) %>%
+      arrange(desc(count))
+  })
+  
+  output$table <-  renderDataTable({
+    datatable(dat_names_agg(), 
+              options = list(paging = FALSE,
+                             searching = FALSE,
+                             orderClasses = TRUE))
+    
+  })
+  
 }
 
 
